15e9932b5d171b196c2097ebff1ed671874b4349
theajack <1506282385@qq.com>
2025-01-14 21:02:12 +0800

    feat: jsbox config

--- jsbox.code.js
+++ jsbox.code.js
@@ -7,6 +7,7 @@ window.jsboxCode = {
     lib: 'https://cdn.jsdelivr.net/npm/web-term-ui',
     lang: 'javascript',
     needUI: true,
+    hideLog: true,
     code: `document.getElementById('jx-app').innerHTML = '<div id="container" style="width: 600px; height: 400px;"></div>';
 var {WebTerm} = window.WebTermUi
 const term = new WebTerm({
